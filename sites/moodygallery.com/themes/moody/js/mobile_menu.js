c4cc8a02d8a2d8128269a0dd09fc02c51fdbca35
Asad Hasan <asad@dadesigners.com>
2015-07-09 13:52:05 -0400

    Implement menu.

--- sites/moodygallery.com/themes/moody/js/mobile_menu.js
+++ sites/moodygallery.com/themes/moody/js/mobile_menu.js
@@ -1,3 +1,8 @@
 Drupal.behaviors.mobile_menu = function(context) {
-	$('.primary_nav', context).prepend('<a href="#">Menu</a>');
+	var $primar_nav = $('.primary_nav', context);
+	$primar_nav.prepend('<a class="mobile-menu-trigger" href="#">Menu</a>');
+	$('a.mobile-menu-trigger', context).click(function(e) {
+		e.preventDefault();
+		$primar_nav.find('ul').toggleClass('menuExpanded');
+	});
 }
