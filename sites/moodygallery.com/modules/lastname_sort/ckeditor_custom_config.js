74690e0af202ca168a53fc06992c4f74fd4aab01
Asad Hasan <asad@dadesigners.com>
2015-12-16 13:08:07 -0600

    Attempt hack for heveltica.

---
+++ sites/moodygallery.com/modules/lastname_sort/ckeditor_custom_config.js
@@ -0,0 +1,7 @@
+CKEDITOR.editorConfig = function( config )
+{
+//       config.contentsCss = '/sites/all/libraries/ckeditor/customFonts/fonts.css';
+//       config.font_names = 'Helvetica Regular/Helvetica-Regular;' + config.font_names;
+//       config.font_names = 'Helvetica Bold/Helvetica-Bold;' + config.font_names;
+//       config.font_names = 'Helvetica Light/Helvetica-Light;' + config.font_names;
+};
